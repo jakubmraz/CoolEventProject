b0d5592e97741c9a47e9b66f563e3518a37442e2
Rania Zarka <rani0401@edu.easj.dk>
2020-02-03 12:58:21 +0100

    phase 7

--- ViewModel/EventViewModel.cs
+++ ViewModel/EventViewModel.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using CoolEventProject.Model;
 
 namespace CoolEventProject.ViewModel
 {
     class EventViewModel
     {
+        public EventViewModel()
+        {
+            EventCatalogSingleton = EventCatalogSingleton.Instance;
+        }
+
+        public EventCatalogSingleton EventCatalogSingleton { get; }
     }
+
 }
