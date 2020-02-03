cb61b0487bba5005e9564ba62b6e65497fd7598a
Jakub Mraz <jaku0899@edu.easj.dk>
2020-02-03 12:45:18 +0100

    Task 6

---
+++ Model/EventCatalogSingleton.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoolEventProject.Model
+{
+    class EventCatalogSingleton
+    {
+        private EventCatalogSingleton _instance;
+
+        private EventCatalogSingleton()
+        {
+            AddEvent(01, "Test Event", "This event is purely hypothetical.", "Virtual", DateTime.Today);
+            AddEvent(02, "Test Event #2", "Because one test was not enough.", "Virtual", DateTime.Today);
+        }
+
+        public EventCatalogSingleton Instance
+        {
+            get
+            {
+                if (_instance != null)
+                {
+                    return _instance;
+                }
+                else
+                {
+                    return new EventCatalogSingleton();
+                }
+            }
+        }
+
+        public ObservableCollection<Event> EventCollection = new ObservableCollection<Event>();
+
+        public void AddEvent(int id, string name, string description, string place, DateTime dateTime)
+        {
+            EventCollection.Add(new Event(id, name, description, place, dateTime));
+        }
+    }
+}
