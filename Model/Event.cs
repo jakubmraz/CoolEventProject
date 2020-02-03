53c3cadd680a2af99b1983b1708d31fd4b4a546c
Jakub Mraz <jaku0899@edu.easj.dk>
2020-02-03 12:32:38 +0100

    Task 5

--- Model/Event.cs
+++ Model/Event.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.ApplicationModel.Appointments.AppointmentsProvider;
 
 namespace CoolEventProject.Model
 {
     class Event
     {
+
+        public Event(int id, string name, string description, string place, DateTime dateTime)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Place = place;
+            DateTime = dateTime;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Place { get; set; }
+        public DateTime DateTime { get; set; }
+
+        public override string ToString()
+        {
+            return $"Event Name: {Name}; Event ID: {Id}; Description: {Description}; Place: {Place}; Scheduled Date: {DateTime}";
+        }
+
     }
 }
